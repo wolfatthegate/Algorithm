c0a77b0470170f8ff4aaddd77c615ab6144223e7
Wolf Walker <58965905+wolfatthegate@users.noreply.github.com>
2021-10-23 16:21:05 -0400

    Update Fall_2015_Q2.py

--- Fall_2015_Q2.py
+++ Fall_2015_Q2.py
@@ -2,17 +2,13 @@
 Algorithm November 2015
 Problem 2
 
-
 Let I1, . . . , In be n intervals, where interval Ii is defined by set [ai, bi], i.e., starting from ai and
 ending at bi. The interval scheduling problem asks to find a maximum number of disjoint intervals
 that do not overlap with each other (for example, if four intervals are given as I1 = [1, 2], I2 =
 [2, 3], I3 = [1, 4], I4 = [4, 5], then the solution is {I1, I2, I4}). Can you design a linear time greedy
 algorithm to solve the interval scheduling problem? You can ignore the running time of your
 algorithm in the pre-processing phase.
-
-
 '''
-
 
 I = [[1,2], [2,3], [1,4], [4, 5]]
 
@@ -25,9 +21,7 @@ time = 0
 job_list = []
 
 while len(I) > i: 
-    
     temp = I[i]
-    
     if temp[0] >= time: 
         job_list.append(temp)
         time = temp[1]
