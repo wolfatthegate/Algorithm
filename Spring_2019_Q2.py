3e581eef863d98e4221e195d9768e1f8e2807595
Wolf Walker <58965905+wolfatthegate@users.noreply.github.com>
2021-10-23 02:03:32 -0400

    Create Spring_2019_Q2.py

---
+++ Spring_2019_Q2.py
@@ -0,0 +1,39 @@
+'''
+ Preliminary Exam Algorithm April 2019
+    
+ Question 2 
+
+ *** use greedy algorithm
+ 
+ - sort the job by highest profit
+ - pick up the highest profit job and check the deadline
+ - if the job meets the deadline, add it to the queue
+ - update the time
+ - repeat the last three steps. 
+ 
+ Author 
+ @WaylonLuo 
+
+'''
+#   each list has [processing time, maximum profit, deadline]
+#   [a[0]    ,a[1]   ,a[2]   ,a[3]   ,a[4]    ]
+#    
+a = [[5,3,10],[4,6,9],[7,2,5],[3,4,8],[8,3,16]]
+
+### sorting will take O(n log n) time
+sorted_a = sorted(a, key = lambda x: x[1])
+print('available jobs:', sorted_a)
+
+jobs = []
+time = 0 
+
+### while loop takes O(n) time for every jobs. 
+while len(sorted_a) > 0: 
+    temp = sorted_a.pop()
+    if time < temp[2]: 
+        jobs.append(temp)
+        time += temp[0]
+
+print('eligible jobs :', jobs)
+print('maximum profit: ', sum(j[1] for j in jobs))
+
