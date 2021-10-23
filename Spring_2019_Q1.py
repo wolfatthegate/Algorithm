46c15e125a30de013788dffe41498a4f18a4107d
Wolf Walker <58965905+wolfatthegate@users.noreply.github.com>
2021-10-23 02:06:11 -0400

    Create Spring_2019_Q1.py

---
+++ Spring_2019_Q1.py
@@ -0,0 +1,18 @@
+'''
+ Preliminary Exam Algorithm April 2019
+ Question 1
+ Inversion 
+
+'''
+
+#   [0, 1, 2, 3, 4]
+a = [2, 3, 8, 6, 1]
+
+invr = []
+
+for i in range(0, len(a)):
+    for j in range(i+1, len(a)):
+        if i < j and a[i] > a[j]:
+            invr.append([i, j])
+
+print(invr)
