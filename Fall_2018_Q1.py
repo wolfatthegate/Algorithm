5d26a9ae74bec5ff265aeb74d87de1cd5f49f344
Wolf Walker <58965905+wolfatthegate@users.noreply.github.com>
2021-10-23 02:36:22 -0400

    Create Fall_2018_Q1.py

---
+++ Fall_2018_Q1.py
@@ -0,0 +1,27 @@
+'''
+Algorithm November 2018 
+
+Problem #1
+
+Moore's algorithm only works if the majority sample 
+is larger than 50% of the entire sample
+
+'''
+
+lst = [8, 4, 8, 5, 8, 8, 3, 4, 8, 8, 5, 8, 7, 8]
+
+n = len(lst)
+res = 0
+count = 1
+
+for i in range(1, n-1):
+    if lst[res] == lst[i+1]:
+        count += 1
+    else:
+        count -= 1
+    
+    if count == 0:
+        res = i+1
+        count += 1
+    
+print('the most frequent number is {}'.format(lst[res]))
