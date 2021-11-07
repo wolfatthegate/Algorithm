c81a178e7a160e44c3da99d51b1fb7fe94062ca5
Wolf Walker <58965905+wolfatthegate@users.noreply.github.com>
2021-11-07 16:15:03 -0500

    Create Fall_2016_Q2.py

---
+++ Fall_2016_Q2.py
@@ -0,0 +1,19 @@
+Question #2:
+Show the hash table after you insert the keys 5, 28, 19, 15, 20, 33, 12, 17, 10. Show the same hash
+table after collisions are resolved by chaining. Moreover, show the hash table after inserting keys
+using open addressing. Let the table have 9 slots and let the hash function be h(k) = k mod 9.
+
+
+
+
+| key | h(k) = k mod 9   | 
+| --- | ---------------- |
+| 5   | h(5) = 5 mod 9   |
+| 28  | h(28) = 28 mod 9 |
+| 19  | h(19) = 19 mod 9 |
+| 15  | h(15) = 15 mod 9 |
+| 20  | h(20) = 20 mod 9 |
+| 33  | h(33) = 33 mod 9 |
+| 12  | h(12) = 12 mod 9 |
+| 17  | h(17) = 17 mod 9 |
+| 10  | h(10) = 10 mod 9 |
